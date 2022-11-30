4478a08fa0f3d14112e27c78beca84dee9ae9073
Spandananu <Spandananukala@gmail.com>
2022-11-30 22:35:55 +0530

    ok

---
+++ Java Script/EX.JS/operators1/Addition.js
@@ -0,0 +1,16 @@
+let a=10;
+let b=20;
+let c=30.5
+let d="Bharath";
+let e="Lenkalapally"
+let f=true;
+let g;
+
+console.log(a+b) //30
+console.log(a+c) //40.5
+console.log(a+d) //10Bharath
+console.log(a+e) //10Lenkalapally
+console.log(a+f) //11
+console.log(a+g) //NaN
+
+
---
+++ Java Script/EX.JS/operators1/multiplication.js
@@ -0,0 +1,14 @@
+let a=10;
+let b=20;
+let c=30.5
+let d="Bharath";
+let e="Lenkalapally"
+let f=true;
+let g;
+
+console.log(a*b) //200
+console.log(a*c) //305
+console.log(a*d) //NaN
+console.log(a*e) //NaN
+console.log(a*f) //10
+console.log(a*g) //NaN
