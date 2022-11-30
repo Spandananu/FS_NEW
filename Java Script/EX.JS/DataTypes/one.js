de2295dd914ae8e91911ba0706ea7e4ce7493340
Spandananu <Spandananukala@gmail.com>
2022-11-30 17:01:09 +0530

    ok

---
+++ Java Script/EX.JS/DataTypes/one.js
@@ -0,0 +1,14 @@
+let price=14999;
+let rating=4.9;
+let name="IPhone 5s";
+let avail='Y';
+let Discount=true;
+let availability;
+
+console.log(price)
+console.log(typeof price)
+console.log(typeof rating)
+console.log(typeof name)
+console.log(typeof avail)
+console.log(typeof Discount)
+console.log(typeof availability)
