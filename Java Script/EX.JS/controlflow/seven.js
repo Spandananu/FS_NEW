e148f4f7325ec71ff27dbc54f7ea2d695473319d
Spandananu <Spandananukala@gmail.com>
2022-12-07 12:03:38 +0530

    js

---
+++ Java Script/EX.JS/controlflow/seven.js
@@ -0,0 +1,9 @@
+let amount=0;
+if (amount){
+    console.log("Go to movie HIT-2")
+}
+else {
+    console.log("Go to PG ! Open Laptop and Practise")
+}
+
+    
---
+++ Java Script/EX.JS/controlflow/six.js
@@ -0,0 +1,7 @@
+let amount = 1000;
+if (amount = 0) {
+    console.log("Go to Movie! HIT-2")
+}
+else {
+    console.log("Go to PG ! Open Laptop and Practise")
+}
