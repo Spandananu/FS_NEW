6c89176787116813e0aea93841940199c4df9292
Spandananu <Spandananukala@gmail.com>
2022-12-08 20:12:16 +0530

    js

---
+++ Java Script/EX.JS/Array two/contact.js
@@ -0,0 +1,9 @@
+let contacts =[{id:101, name : "spandana"},
+{id:102, name :"bharath"},
+{id:103, name:"vignesh"}]
+
+for(contact of contacts){
+    //console.log(contact)
+    console.log(contact.name)
+    console.log(contact.id)
+}
