eaa0c4b66aff537a21cd4115ef2f97a9ce203f7f
Spandananu <Spandananukala@gmail.com>
2022-12-08 20:32:32 +0530

    js

---
+++ Java Script/EX.JS/Function/three.js
@@ -0,0 +1,14 @@
+function add(a,b){
+//function with parameters
+console.log(a+b)
+}
+add(10,20)           //30
+add(100,200)         //300
+add(1,"spandana")    //1spandana
+add(1)               //NaN
+
+
+
+
+
+
