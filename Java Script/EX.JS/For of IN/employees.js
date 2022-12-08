0c8d06c63935bb2ff681b0b1aac9db40318f97ef
Spandananu <Spandananukala@gmail.com>
2022-12-08 12:32:31 +0530

    js

---
+++ Java Script/EX.JS/For of IN/employees.js
@@ -0,0 +1,17 @@
+ let employees=["spandana","bharath","rishitha","vignesh","krishnaveni"]
+ //index            0           1         2          3        4
+ //iterate array using whileloop
+ /*
+ initilazation  : let i=0
+ condition st   : let i<=employees.length-1
+ incre/decre st :i++
+ */
+
+ /* let i=0;
+ while (i<=employees.length-1){
+    console.log(employees[i])
+    i++
+ } */
+ for (employee of employees){
+    console.log(employee)
+ }
